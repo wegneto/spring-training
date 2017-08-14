ba26a68ccbd7c8001a3d2b9cf47c4372c4f43203
Wilson Guimarães <wegneto@gmail.com>
2017-08-14 14:55:55 +0100

    Step 1 - Setting up a Spring Project using htttp://start.spring.io

---
+++ in28minutes/spring-in-5-steps/src/main/java/com/wegneto/springin5steps/SpringIn5StepsApplication.java
@@ -0,0 +1,12 @@
+package com.wegneto.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+}
