8b66fd6488f99c8afc34c21e1ae2c4b5565a2278
Wilson Guimarães <wegneto@gmail.com>
2017-11-23 23:39:01 +0000

    Create java project

--- in28minutes/eclipse-in-5-steps/src/HelloWorldNew.java
+++ in28minutes/eclipse-in-5-steps/src/HelloWorldNew.java
@@ -1,5 +1,19 @@
-public class HelloWorld {	
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorldNew {
+	private static Integer number;
+	private Integer number2;
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		BigDecimal bigDecimal2 = new BigDecimal("500.1");
+		number = new Integer(1);
+		
+		Map<String, String> map = new HashMap<>();
+	}
+
+	public void newMethod() {
+		number2 = new Integer(2);
 	}
 }
