c2c1bc07174acdcf360446355745991c8381797d
Wilson Guimarães <wegneto@gmail.com>
2017-11-24 00:30:20 +0000

    Step 1: Creating and importing a Maven Project

---
+++ in28minutes/maven-in-5-steps/src/main/java/com/wegneto/learning/maven/mavenin5steps/MavenIn5StepsApplication.java
@@ -0,0 +1,12 @@
+package com.wegneto.learning.maven.mavenin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenIn5StepsApplication.class, args);
+	}
+}
---
+++ in28minutes/maven-in-5-steps/src/test/java/com/wegneto/learning/maven/mavenin5steps/MavenIn5StepsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wegneto.learning.maven.mavenin5steps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenIn5StepsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
