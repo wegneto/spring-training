45bb761387e33d995b2dc3d6fcaeef17e0dcd01b
Wilson Guimarães <wegneto@gmail.com>
2017-12-05 22:22:26 +0000

    Step 14 - Scope of a Bean - Prototype and Singleton

--- in28minutes/spring-in-5-steps/src/main/java/com/wegneto/springin5steps/BinarySearchImpl.java
+++ in28minutes/spring-in-5-steps/src/main/java/com/wegneto/springin5steps/BinarySearchImpl.java
@@ -2,9 +2,12 @@ package com.wegneto.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
 	
 	@Autowired
--- in28minutes/spring-in-5-steps/src/main/java/com/wegneto/springin5steps/SpringIn5StepsApplication.java
+++ in28minutes/spring-in-5-steps/src/main/java/com/wegneto/springin5steps/SpringIn5StepsApplication.java
@@ -3,7 +3,6 @@ package com.wegneto.springin5steps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
@@ -16,6 +15,12 @@ public class SpringIn5StepsApplication {
 		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
 		
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		
+		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+		
+		System.out.println(binarySearch);
+		System.out.println(binarySearch1);
+		
 		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3); 
 		System.out.println(result);
 	}
