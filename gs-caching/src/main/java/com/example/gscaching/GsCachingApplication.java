037c9234170f813ece71cfe56099fed59cbcfa42
Wilson Guimarães <wegneto@gmail.com>
2017-12-08 11:59:59 +0000

    Creating the directory structure

---
+++ gs-caching/src/main/java/com/example/gscaching/GsCachingApplication.java
@@ -0,0 +1,12 @@
+package com.example.gscaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsCachingApplication.class, args);
+	}
+}
