d63d54c4be9ce3d45aaff41c4fd6a98d46bcfc01
Wilson Guimarães <wegneto@gmail.com>
2017-12-09 17:09:00 +0000

    Step 2 : First JUnit Project and Green Bar

---
+++ in28minutes/junit-in-5-steps/src/com/wegneto/junit/MyMath.java
@@ -0,0 +1,12 @@
+package com.wegneto.junit;
+
+public class MyMath {
+	public int sum(int[] numbers) {
+		int sum = 0;
+		for(int i: numbers) {
+			sum += i;
+		}
+		return sum;
+	}
+
+}
---
+++ in28minutes/junit-in-5-steps/test/com/wegneto/junit/MyMathTest.java
@@ -0,0 +1,14 @@
+package com.wegneto.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
