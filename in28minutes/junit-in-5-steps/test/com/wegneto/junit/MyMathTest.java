1c530af4ce153719581c86b8434e2f4bf20549c5
Wilson Guimarães <wegneto@gmail.com>
2017-12-09 17:25:27 +0000

    Step 5 : Important annotations

--- in28minutes/junit-in-5-steps/test/com/wegneto/junit/MyMathTest.java
+++ in28minutes/junit-in-5-steps/test/com/wegneto/junit/MyMathTest.java
@@ -2,19 +2,44 @@ package com.wegneto.junit;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyMathTest {
-	
 	MyMath myMath = new MyMath();
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("@BeforeClass");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("@AfterClass");
+	}
+	
+	@Before
+	public void before() {
+		System.out.println("@before");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("@after");
+	}
 
 	@Test
 	public void sumWith3Numbers() {
+		System.out.println("sumWith3Numbers");
 		assertEquals(6, myMath.sum(new int[] { 1, 2, 3 }));
 	}
 
 	@Test
 	public void sumWith1Number() {
+		System.out.println("sumWith1Number");
 		assertEquals(3, myMath.sum(new int[] { 3 }));
 	}
 
