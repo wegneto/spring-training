0d1d809effbe211b54af8994105a1445e86776fc
Wilson Guimarães <wegneto@gmail.com>
2017-12-09 17:15:09 +0000

    Step 3 : First Code and First Unit Test

--- in28minutes/junit-in-5-steps/test/com/wegneto/junit/MyMathTest.java
+++ in28minutes/junit-in-5-steps/test/com/wegneto/junit/MyMathTest.java
@@ -1,14 +1,21 @@
 package com.wegneto.junit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class MyMathTest {
+	
+	MyMath myMath = new MyMath();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void sumWith3Numbers() {
+		assertEquals(6, myMath.sum(new int[] { 1, 2, 3 }));
+	}
+
+	@Test
+	public void sumWith1Number() {
+		assertEquals(3, myMath.sum(new int[] { 3 }));
 	}
 
 }
