8e0515151aa421197452b69583f0d89d7d8a1549
Wilson Guimarães <wegneto@gmail.com>
2018-01-10 23:36:46 +0000

    Step 03 - Defining an @Before advice

---
+++ in28minutes/spring-aop/src/main/java/com/wegneto/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -0,0 +1,22 @@
+package com.wegneto.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class UserAccessAspect {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Before("execution(* com.wegneto.spring.aop.springaop.business.*.*(..))")
+	public void before(JoinPoint joinPoint) {
+		logger.info("Check for user access");
+		logger.info("Allowed execution for {}", joinPoint);
+	}
+	
+}
