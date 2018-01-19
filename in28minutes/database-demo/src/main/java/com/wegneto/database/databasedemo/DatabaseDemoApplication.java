5adc785335a7a5c1619fa37df841c8928ec7af6d
Wilson Guimarães <wegneto@gmail.com>
2018-01-19 11:40:08 +0000

    Step 12 - Creating a custom Spring JDBC RowMapper

--- in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/DatabaseDemoApplication.java
+++ in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/DatabaseDemoApplication.java
@@ -19,7 +19,7 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PersonJdbcDAO personJdbcDAO;
-
+	
 	public static void main(String[] args){
 		SpringApplication.run(DatabaseDemoApplication.class, args);
 	}
