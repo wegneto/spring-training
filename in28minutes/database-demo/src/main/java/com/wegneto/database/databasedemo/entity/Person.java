413f022fb1ae8525bf2053ea3c3a0dc6cfa28d35
Wilson Guimarães <wegneto@gmail.com>
2018-01-19 15:38:52 +0000

    Step 16 - Implementing insert and update JPA Repository Methods

--- in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/entity/Person.java
+++ in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/entity/Person.java
@@ -18,10 +18,17 @@ public class Person {
 	private Date birthDate;
 	
 	public Person() {}
-
+	
 	public Person(int id, String name, String location, Date birthDate) {
 		super();
 		this.id = id;
+		this.name = name;
+		this.location = location;
+		this.birthDate = birthDate;
+	}
+
+	public Person(String name, String location, Date birthDate) {
+		super();
 		this.name = name;
 		this.location = location;
 		this.birthDate = birthDate;
--- in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/jpa/PersonJpaRepository.java
+++ in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/jpa/PersonJpaRepository.java
@@ -18,4 +18,12 @@ public class PersonJpaRepository {
 	public Person findById(int id) {
 		return entityManager.find(Person.class, id);
 	}
+	
+	public Person update(Person person) {
+		return entityManager.merge(person);
+	}
+	
+	public Person insert(Person person) {
+		return entityManager.merge(person);
+	}
 }
