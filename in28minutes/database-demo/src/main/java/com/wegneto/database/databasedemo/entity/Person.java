fb3e6869dd36dcb63112479031d986fc00538fd6
Wilson Guimarães <wegneto@gmail.com>
2018-01-19 15:08:10 +0000

    Step 14 - Defining Person Entity

--- in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/entity/Person.java
+++ in28minutes/database-demo/src/main/java/com/wegneto/database/databasedemo/entity/Person.java
@@ -2,9 +2,17 @@ package com.wegneto.database.databasedemo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
 	
+	@Id
+	@GeneratedValue
 	private int id;
+	
 	private String name;
 	private String location;
 	private Date birthDate;
