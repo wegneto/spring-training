54214c9bd7fff25d3ffb6bee50d221f51a647ded
Wilson Guimarães <wegneto@gmail.com>
2018-02-03 21:45:59 +0000

    Configurando o Spring MVC

---
+++ devmedia/intro-spring-mvc/src/main/java/com/wegneto/spring/mvc/config/SpringInitConfig.java
@@ -0,0 +1,22 @@
+package com.wegneto.spring.mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringMvcConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
