a84ac655ba3c659e5c94d29c4598ab2ffd7de4f9
Wilson Guimarães <wegneto@gmail.com>
2018-02-08 16:16:29 +0000

    Editando registros.

--- devmedia/intro-spring-mvc/src/main/java/com/wegneto/spring/mvc/web/controller/UsuarioController.java
+++ devmedia/intro-spring-mvc/src/main/java/com/wegneto/spring/mvc/web/controller/UsuarioController.java
@@ -1,10 +1,13 @@
 package com.wegneto.spring.mvc.web.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +40,20 @@ public class UsuarioController {
 		dao.save(usuario);
 		redirectAttributes.addFlashAttribute("message", "Usuário salvo com sucesso.");
 		return "redirect:/usuario/todos";
+	}
+	
+	@GetMapping("/update/{id}")
+	public ModelAndView preUpdate(@PathVariable("id") UUID id, ModelMap model) {
+		Usuario usuario = dao.findById(id);
+		model.addAttribute("usuario", usuario);
+		return new ModelAndView("/user/add", model);
+	}
+	
+	@PostMapping("/update")
+	public ModelAndView update(@ModelAttribute("usuario") Usuario usuario, RedirectAttributes redirectAttributes) {
+		dao.update(usuario);
+		redirectAttributes.addFlashAttribute("message", "Usuário editado com sucesso.");
+		return new ModelAndView("redirect:/usuario/todos");
 	}
 
 }
