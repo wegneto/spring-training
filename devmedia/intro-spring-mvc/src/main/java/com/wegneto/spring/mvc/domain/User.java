21164858c4f6049d708565627704483c492011ee
Wilson Guimarães <wegneto@gmail.com>
2018-02-15 00:09:47 +0000

    Mapeando a classe de entidade.

--- devmedia/intro-spring-mvc/src/main/java/com/wegneto/spring/mvc/domain/User.java
+++ devmedia/intro-spring-mvc/src/main/java/com/wegneto/spring/mvc/domain/User.java
@@ -1,8 +1,16 @@
 package com.wegneto.spring.mvc.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -10,55 +18,41 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-public class User {
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
 
-	private UUID id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
+	@Column(nullable = false, length = 50)
 	@NotBlank
 	@Size(min = 3, max = 50)
 	private String name;
 
+	@Column(nullable = false, length = 50)
 	@NotBlank
 	@Size(min = 3, max = 50, message = "Surname must have between {min} and {max} chars.")
 	private String surname;
 
+	@Column(nullable = false)
 	@NotNull
 	@DateTimeFormat(iso = ISO.DATE)
 	private LocalDate birthday;
 
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Gender gender;
 
 	public User() {
 	}
 
-	public User(UUID id, String name, String surname) {
-		this.id = id;
-		this.name = name;
-		this.surname = surname;
-	}
-
-	public User(UUID id, String name, String surname, LocalDate birthday) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.surname = surname;
-		this.birthday = birthday;
-	}
-
-	public User(UUID id, String name, String surname, LocalDate birthday, Gender gender) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.surname = surname;
-		this.birthday = birthday;
-		this.gender = gender;
-	}
-
-	public UUID getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(UUID id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -97,6 +91,31 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", surname=" + surname + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
