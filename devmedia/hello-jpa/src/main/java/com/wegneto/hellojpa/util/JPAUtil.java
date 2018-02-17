9a70becdcac5c4f918a5253f343a09fe6de2e972
Wilson Guimarães <wegneto@gmail.com>
2018-02-17 18:06:03 +0000

    Configurando recursos JPA.

---
+++ devmedia/hello-jpa/src/main/java/com/wegneto/hellojpa/util/JPAUtil.java
@@ -0,0 +1,29 @@
+package com.wegneto.hellojpa.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private EntityManagerFactory factory;
+	
+	private static JPAUtil instance;
+	
+	private JPAUtil() {
+		this.factory = Persistence.createEntityManagerFactory("hello-jpa");
+	}
+	
+	public static synchronized JPAUtil getInstance() {
+		if (instance == null) {
+			instance = new JPAUtil();
+		}
+		
+		return instance;
+	}
+	
+	public EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
