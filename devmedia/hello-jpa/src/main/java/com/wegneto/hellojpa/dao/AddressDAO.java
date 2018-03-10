cdaccae96257806a09300147f44dd471082073e9
Wilson Guimarães <wegneto@gmail.com>
2018-03-10 21:46:22 +0000

    Aula 16: Mapeamento N x M

---
+++ devmedia/hello-jpa/src/main/java/com/wegneto/hellojpa/dao/AddressDAO.java
@@ -0,0 +1,11 @@
+package com.wegneto.hellojpa.dao;
+
+import com.wegneto.hellojpa.entity.Address;
+
+public class AddressDAO extends GenericDAO<Address> {
+
+	public AddressDAO() {
+		super(Address.class);
+	}
+	
+}
