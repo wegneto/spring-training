adbbdd4447059d9adca5dbda3b2f38ce77f2331a
Wilson Guimarães <wegneto@gmail.com>
2018-03-14 23:48:43 +0000

    Aula 20: criando o projeto

---
+++ devmedia/intro-spring-data/src/main/java/com/wegneto/spring_data/IntroSpringDataApplication.java
@@ -0,0 +1,12 @@
+package com.wegneto.spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringDataApplication.class, args);
+	}
+}
