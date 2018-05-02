54a5db5a138be5d5ecce96913543337fc2510d01
Wilson Guimarães <wegneto@gmail.com>
2018-05-02 18:47:53 +0100

    Aula 30. Keywords: StartingWith, EndingWith e Containing

--- devmedia/intro-spring-data/src/main/java/com/wegneto/spring_data/repository/AddressRepository.java
+++ devmedia/intro-spring-data/src/main/java/com/wegneto/spring_data/repository/AddressRepository.java
@@ -1,9 +1,15 @@
 package com.wegneto.spring_data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wegneto.spring_data.entity.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
+	List<Address> findByCityStartingWith(String city);
+	
+	List<Address> findByStreetEndingWith(String street);
+	
 }
