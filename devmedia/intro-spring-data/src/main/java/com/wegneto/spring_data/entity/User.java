73001b0293a3855f691400804c5caa221d9233d3
Wilson Guimarães <wegneto@gmail.com>
2018-05-22 22:18:01 +0100

    Aula 40. Entidade com AbstractPersistable

---
+++ devmedia/intro-spring-data/src/main/java/com/wegneto/spring_data/entity/User.java
@@ -0,0 +1,35 @@
+package com.wegneto.spring_data.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@Table(name = "users")
+public class User extends AbstractPersistable<Long> {
+	
+	@Column(nullable = false)
+	private String userName;
+	
+	@Column(nullable = false)
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
