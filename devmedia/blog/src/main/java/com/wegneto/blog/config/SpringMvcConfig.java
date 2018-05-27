13586f8ebac97ee3613f4afef8d78652fd1b9def
Wilson Guimarães <wegneto@gmail.com>
2018-05-27 12:20:58 +0100

    Aula 44. Configurando o Spring MVC

--- devmedia/blog/src/main/java/com/wegneto/blog/config/SpringMvcConfig.java
+++ devmedia/blog/src/main/java/com/wegneto/blog/config/SpringMvcConfig.java
@@ -1,5 +1,36 @@
 package com.wegneto.blog.config;
 
-public class SpringMvcConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.wegneto.blog")
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean(name = "jspViewResolver")
+	public InternalResourceViewResolver resourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+
+		return resolver;
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setMaxUploadSize(100_000); // 100k
+
+		return resolver;
+	}
 
 }
---
+++ devmedia/blog/src/main/java/com/wegneto/blog/config/SpringWebXmlConfig.java
@@ -0,0 +1,34 @@
+package com.wegneto.blog.config;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringWebXmlConfig implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		webContext.register(SpringMvcConfig.class);
+		webContext.setServletContext(servletContext);
+
+		DispatcherServlet dispatcher = new DispatcherServlet(webContext);
+		dispatcher.setThrowExceptionIfNoHandlerFound(true);
+
+		ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher", dispatcher);
+		dynamic.setLoadOnStartup(1);
+		dynamic.addMapping("/");
+		
+		FilterRegistration.Dynamic encondingFilter = servletContext.addFilter("encondingFilter", new CharacterEncodingFilter());
+		encondingFilter.setInitParameter("encoding", "UTF-8");
+		encondingFilter.setInitParameter("forceEncoding", "true");
+		encondingFilter.addMappingForUrlPatterns(null, true, "/*");
+	}
+
+}
