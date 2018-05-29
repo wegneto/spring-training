c310db8d75e3ec6e71ed526b7970c20fac59a299
Wilson Guimarães <wegneto@gmail.com>
2018-05-29 13:44:11 +0100

    Aula 47. Iniciando o controller para usuário

--- devmedia/blog/src/main/java/com/wegneto/blog/service/UsuarioService.java
+++ devmedia/blog/src/main/java/com/wegneto/blog/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.wegneto.blog.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +34,22 @@ public class UsuarioService {
 	
 	public Usuario findById(Long id) {
 		return repository.findOne(id);
+	}
+	
+	@Transactional(readOnly = false)
+	public void delete(Long id) {
+		repository.delete(id);
+	}
+	
+	@Transactional(readOnly = false)
+	public void save(Usuario usuario) {
+		if (usuario.getDataCadastro() == null) {
+			usuario.setDataCadastro(LocalDate.now());
+		}
+		
+		String hash = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		
+		repository.save(usuario);
 	}
 
 }
---
+++ devmedia/blog/src/main/java/com/wegneto/blog/web/controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package com.wegneto.blog.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wegneto.blog.entity.Avatar;
+import com.wegneto.blog.entity.Usuario;
+import com.wegneto.blog.service.AvatarService;
+import com.wegneto.blog.service.UsuarioService;
+
+@Controller
+@RequestMapping("usuario")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private AvatarService avatarService;
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public ModelAndView showForm(@ModelAttribute("usuario") Usuario usuario) {
+		return new ModelAndView("usuario/cadastro");
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@ModelAttribute("usuario") Usuario usuario,
+			@RequestParam(value = "file", required = false) MultipartFile file) {
+		Avatar avatar = avatarService.getAvatarByUpload(file);
+		usuario.setAvatar(avatar);
+		usuarioService.save(usuario);
+		
+		return "redirect:/usuario/perfil/" + usuario.getId();
+	}
+
+}
