48023e44791c1c3892a6f4503dc2dc192c238e71
Wilson Guimarães <wegneto@gmail.com>
2018-06-16 23:48:16 +0100

    Fix name.

--- devmedia/blog/src/main/java/com/wegneto/blog/repository/PostagemRepository.java
+++ devmedia/blog/src/main/java/com/wegneto/blog/repository/PostagemRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wegneto.blog.entity.Postagem;
 
-public interface PostagemRespository extends JpaRepository<Postagem, Long>{
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 	Postagem findByPermalink(String permalink);
 	
