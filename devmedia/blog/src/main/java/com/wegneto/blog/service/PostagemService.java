77f7e082e7d4de7489a763f5a4482a96dcc3828f
Wilson Guimarães <wegneto@gmail.com>
2018-06-16 23:59:42 +0100

    Aula 63. O service para Postagem

---
+++ devmedia/blog/src/main/java/com/wegneto/blog/service/PostagemService.java
@@ -0,0 +1,54 @@
+package com.wegneto.blog.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wegneto.blog.entity.Postagem;
+import com.wegneto.blog.repository.PostagemRepository;
+import com.wegneto.blog.util.MyReplaceString;
+
+@Service
+@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
+public class PostagemService {
+
+	@Autowired
+	private PostagemRepository repository;
+	
+	@Transactional(readOnly = false)
+	public void saveOrUpdate(Postagem postagem) {
+		if (postagem.getId() == null) {
+			save(postagem);
+		} else {
+			update(postagem);
+		}
+	}
+	
+	private void save(Postagem postagem) {
+		postagem.setPermalink(MyReplaceString.formatarPermalink(postagem.getTitulo()));
+		postagem.setDataPostagem(LocalDateTime.now());
+		repository.save(postagem);
+		
+	}
+
+	private void update(Postagem postagem) {
+		
+	}
+
+	public Postagem findById(Long id) {
+		return repository.findOne(id);
+	}
+	
+	public Postagem findByPermalink(String permalink) {
+		return repository.findByPermalink(permalink);
+	}
+	
+	public List<Postagem> findAll() {
+		return repository.findAll();
+	}
+	
+}
---
+++ devmedia/blog/src/main/java/com/wegneto/blog/util/MyReplaceString.java
@@ -0,0 +1,18 @@
+package com.wegneto.blog.util;
+
+import java.text.Normalizer;
+
+public class MyReplaceString {
+
+	public static String formatarPermalink(String val) {
+		String link = val.trim();
+		link = link.toLowerCase();
+		link = Normalizer.normalize(link, Normalizer.Form.NFD);
+		link = link.replaceAll("\\s", "_");
+		link = link.replaceAll("\\_+", "_");
+		link = link.replaceAll("\\W", "");
+		
+		return link;
+	}
+	
+}
