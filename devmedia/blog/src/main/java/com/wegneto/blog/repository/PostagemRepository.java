2888fa94bcac45b166afe9a98a2744822383f458
Wilson Guimarães <wegneto@gmail.com>
2018-06-26 23:54:13 +0100

    [wip]

--- devmedia/blog/src/main/java/com/wegneto/blog/repository/PostagemRepository.java
+++ devmedia/blog/src/main/java/com/wegneto/blog/repository/PostagemRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wegneto.blog.entity.Postagem;
 
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 	Postagem findByPermalink(String permalink);
 
 	List<Postagem> findByCategoriasPermalink(String link);
 
 	List<Postagem> findByAutorNome(String nome);
-	
+
 }
