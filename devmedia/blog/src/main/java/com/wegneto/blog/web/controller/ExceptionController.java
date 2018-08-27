841695889cbf70fe18c492ee84db6f2c54a39c2b
Wilson Guimarães <wegneto@gmail.com>
2018-08-28 00:05:48 +0100

    Aula 100. Exceções a nível de controller

--- devmedia/blog/src/main/java/com/wegneto/blog/web/controller/ExceptionController.java
+++ devmedia/blog/src/main/java/com/wegneto/blog/web/controller/ExceptionController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -13,6 +14,16 @@ public class ExceptionController {
 	public ModelAndView genericException(HttpServletRequest request, Exception exception) {
 		ModelAndView view = new ModelAndView("error");
 		view.addObject("mensagem", "Ocorreu um erro durante a operação. Tente novamente.");
+		view.addObject("url", request.getRequestURL());
+		view.addObject("excecao", exception);
+		
+		return view;
+	}
+	
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ModelAndView genericException(HttpServletRequest request, MaxUploadSizeExceededException exception) {
+		ModelAndView view = new ModelAndView("error");
+		view.addObject("mensagem", "Ocorreu um erro durante a operação: o arquivo ultrapassa o tamanho permitido. Tente novamente.");
 		view.addObject("url", request.getRequestURL());
 		view.addObject("excecao", exception);
 		
