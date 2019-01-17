53e43647e6f906d8287249aaebcbb5795dc906ba
Wilson Guimarães <wegneto@gmail.com>
2019-01-17 15:18:02 +0000

    S04L20 - Code Demo - Rough Prototype Part 2

--- spring-demo-one/src/com/wegneto/springdemo/MyApp.java
+++ spring-demo-one/src/com/wegneto/springdemo/MyApp.java
@@ -3,7 +3,7 @@ package com.wegneto.springdemo;
 public class MyApp {
 	
 	public static void main(String[] args) {
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		
 		System.out.println(theCoach.getDailyWorkout());
 	}
---
+++ spring-demo-one/src/com/wegneto/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.wegneto.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "This is your today's Track workout";
+	}
+
+}
