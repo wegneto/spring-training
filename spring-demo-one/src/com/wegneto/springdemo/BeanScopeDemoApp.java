b051dcf8efc3dec52813d99af83e20a71e4c8b7d
Wilson Guimarães <wegneto@gmail.com>
2019-01-22 20:11:04 +0000

    Lecture 45 - Bean Scopes - Write Some Code - Part 2

--- spring-demo-one/src/com/wegneto/springdemo/BeanScopeDemoApp.java
+++ spring-demo-one/src/com/wegneto/springdemo/BeanScopeDemoApp.java
@@ -6,10 +6,22 @@ public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
-		
+
 		Coach coach1 = context.getBean("myCoach", Coach.class);
-		
+
 		Coach coach2 = context.getBean("myCoach", Coach.class);
+
+		// check if they are the same
+		boolean result = (coach1 == coach2);
+
+		// print out the results
+		System.out.println("Pointing to the same object: " + result);
+
+		System.out.println("Memory location for coach1: " + coach1);
+		System.out.println("Memory location for coach2: " + coach2);
+		
+		context.close();
+		
 	}
-	
+
 }
