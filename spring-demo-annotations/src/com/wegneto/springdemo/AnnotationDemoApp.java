436727a350779d04ce8beb92445ae366c8149097
Wilson Guimarães <wegneto@gmail.com>
2019-02-11 22:53:07 +0000

    Lecture 61. Constructor Injection - Write Some Code - Part 2

--- spring-demo-annotations/src/com/wegneto/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/wegneto/springdemo/AnnotationDemoApp.java
@@ -10,6 +10,7 @@ public class AnnotationDemoApp {
 		Coach coach = context.getBean(TennisCoach.class);
 		
 		System.out.println(coach.getDailyWorkout());
+		System.out.println(coach.getDailyFortune());
 		
 		context.close();
 	}
--- spring-demo-annotations/src/com/wegneto/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/wegneto/springdemo/TennisCoach.java
@@ -1,13 +1,26 @@
 package com.wegneto.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "This is your Tennis workout.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
