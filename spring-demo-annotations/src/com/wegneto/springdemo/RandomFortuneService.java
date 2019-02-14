e6172c1d356be7862408aded696540a467440a52
Wilson Guimarães <wegneto@gmail.com>
2019-02-14 23:43:35 +0000

    Lecture 71. Qualifiers for Dependency Injection

---
+++ spring-demo-annotations/src/com/wegneto/springdemo/RandomFortuneService.java
@@ -0,0 +1,23 @@
+package com.wegneto.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = { "Fortune 1", "Fortune 2", "Fortune 3" };
+	
+	private Random myRandom = new Random();
+
+	@Override
+	public String getFortune() {
+		int index = myRandom.nextInt(data.length);
+		
+		String theFortune = data[index];
+		
+		return theFortune;
+	}
+
+}
