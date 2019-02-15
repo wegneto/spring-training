280b0df5771e90d2944eef7e8a214ebfe1ef0776
Wilson Guimarães <wegneto@gmail.com>
2019-02-15 19:48:13 +0000

    Lecture 77 - @Scope Annotation

---
+++ spring-demo-annotations/src/com/wegneto/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,24 @@
+package com.wegneto.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+	
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach coachOne = context.getBean("tennisCoach", Coach.class);
+		Coach coachTwo = context.getBean("tennisCoach", Coach.class);
+		
+		boolean result = (coachOne == coachTwo);
+		
+		System.out.println("Pointing to the same object: " + result);
+		
+		System.out.println("Memory location for coachOne: " + coachOne);
+		
+		System.out.println("Memory location for coachTwo: " + coachTwo);
+		
+		context.close();
+	}
+
+}
