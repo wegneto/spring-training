d903a40bfa8ec7a44c33b19bc160eef8521c5b29
Wilson Guimarães <wegneto@gmail.com>
2019-02-17 19:05:18 +0000

    Lecture 85. Spring Configuration with Java Code (no xml)

---
+++ spring-demo-annotations/src/com/wegneto/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.wegneto.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.wegneto.springdemo")
+public class SportConfig {
+
+}
