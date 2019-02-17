214008b18306fa8bf37a4e2a1e92adf049236ed8
Wilson Guimarães <wegneto@gmail.com>
2019-02-17 19:43:45 +0000

    Lecture 92. Injecting Values from Properties File

--- spring-demo-annotations/src/com/wegneto/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/wegneto/springdemo/SwimCoach.java
@@ -1,9 +1,17 @@
 package com.wegneto.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
-	
+
 	private FortuneService fortuneService;
-	
+
+	@Value("${foo.email}")
+	private String email;
+
+	@Value("${foo.team}")
+	private String team;
+
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
@@ -16,6 +24,14 @@ public class SwimCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 }
--- spring-demo-annotations/src/com/wegneto/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/wegneto/springdemo/SwimJavaConfigDemoApp.java
@@ -7,10 +7,12 @@ public class SwimJavaConfigDemoApp {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
-		Coach coach = context.getBean("swimCoach", Coach.class);
+		SwimCoach coach = context.getBean(SwimCoach.class);
 		
 		System.out.println(coach.getDailyWorkout());
 		System.out.println(coach.getDailyFortune());
+		System.out.println(coach.getEmail());
+		System.out.println(coach.getTeam());
 		
 		context.close();
 	}
