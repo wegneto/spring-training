7017cd19e10937b423fc8d8542215db3c6877abb
Wilson Guimarães <wegneto@gmail.com>
2019-02-17 19:27:26 +0000

    Lecture 88. Defining Spring Beans with Java Code

--- spring-demo-annotations/src/com/wegneto/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/wegneto/springdemo/SportConfig.java
@@ -1,10 +1,20 @@
 package com.wegneto.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.wegneto.springdemo")
 public class SportConfig {
+	
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 
 }
