b540010c9308fc2daa40bf8c440a035c5ebc6df7
Wilson Guimarães <wegneto@gmail.com>
2019-02-17 19:35:42 +0000

    Lecture 90. Injecting Values from Properties File

--- spring-demo-annotations/src/com/wegneto/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/wegneto/springdemo/SportConfig.java
@@ -1,10 +1,11 @@
 package com.wegneto.springdemo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	
 	@Bean
