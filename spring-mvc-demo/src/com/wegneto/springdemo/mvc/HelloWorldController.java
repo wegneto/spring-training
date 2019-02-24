603fb7a1682ff941eb17e199902fa771d943730d
Wilson Guimarães <wegneto@gmail.com>
2019-02-24 13:35:26 +0000

    114. Adding Data to the Spring Model

--- spring-mvc-demo/src/com/wegneto/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/wegneto/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.wegneto.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +16,18 @@ public class HelloWorldController {
 	
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/toUpperCase")
+	public String toUpperCase(HttpServletRequest request, Model model) {
+		String name = request.getParameter("studentName");
+		name = name.toUpperCase();
+		
+		String result = "toUpperCase = " + name;
+		
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 
