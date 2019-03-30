484cacbdd572d5a5cde9832f5602cdfdd01488be
Wilson Guimarães <wegneto@gmail.com>
2019-03-30 00:11:29 +0000

    Lecture 151. Add Pre-processing Code with @InitBinder

--- spring-mvc-demo/src/com/wegneto/springdemo/mvc/controller/CustomerController.java
+++ spring-mvc-demo/src/com/wegneto/springdemo/mvc/controller/CustomerController.java
@@ -2,9 +2,12 @@ package com.wegneto.springdemo.mvc.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +17,12 @@ import com.wegneto.springdemo.mvc.model.Customer;
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
 		model.addAttribute("customer", new Customer());
@@ -22,7 +31,7 @@ public class CustomerController {
 	
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
-		System.out.println("Last name: | " + customer.getLastName() + "|");
+		System.out.println("Last name: |" + customer.getLastName() + "|");
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
