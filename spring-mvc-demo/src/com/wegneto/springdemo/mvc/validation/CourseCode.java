c63682ec886112658f0c2fec5961e65fd7dce883
Wilson Guimarães <wegneto@gmail.com>
2019-03-31 19:09:16 +0100

    Lecture 164. Creating a Custom Java Annotation

--- spring-mvc-demo/src/com/wegneto/springdemo/mvc/validation/CourseCode.java
+++ spring-mvc-demo/src/com/wegneto/springdemo/mvc/validation/CourseCode.java
@@ -6,10 +6,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = CourseCodeConstraintValidator.class)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
+
+	public String value() default "ABC";
+
+	public String message() default "must start with ABC";
+
+	public Class<?>[] groups() default {};
+
+	public Class<? extends Payload>[] payload() default {};
 
 }
