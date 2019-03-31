e235477032fea653e80fd72c68668163d6ad434f
Wilson Guimarães <wegneto@gmail.com>
2019-03-31 19:14:05 +0100

    Lecture 165. Developing the ConstraintValidator

--- spring-mvc-demo/src/com/wegneto/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/com/wegneto/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -1,5 +1,21 @@
 package com.wegneto.springdemo.mvc.validation;
 
-public class CourseCodeConstraintValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+	private String coursePrefix;
+
+	@Override
+	public void initialize(CourseCode courseCode) {
+		coursePrefix = courseCode.value();
+	}
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		boolean result = value.startsWith(coursePrefix);
+		return result;
+	}
 
 }
