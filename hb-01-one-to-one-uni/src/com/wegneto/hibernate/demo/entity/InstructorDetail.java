1fa38dd5960e5b9e644bc0676e900a1401e68eee
Wilson Guimarães <wegneto@gmail.com>
2019-11-10 17:57:54 +0000

    Lecture 210. @OneToOne - Write Some Code - Create InstructorDetail class

---
+++ hb-01-one-to-one-uni/src/com/wegneto/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,63 @@
+package com.wegneto.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column
+	private String youtubeChannel;
+
+	@Column
+	private String hobby;
+
+	public InstructorDetail() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		super();
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+
+}
