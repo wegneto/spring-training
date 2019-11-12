511e00028aaad506f847fcce2333aaa14436a1ad
Wilson Guimarães <wegneto@gmail.com>
2019-11-12 23:30:29 +0000

    Lecture 225. @OneToMany - Bi-Directional - Create Course Mapping

---
+++ hb-03-one-to-many/src/com/wegneto/hibernate/demo/CreateDemo.java
@@ -0,0 +1,44 @@
+package com.wegneto.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.wegneto.hibernate.demo.entity.Instructor;
+import com.wegneto.hibernate.demo.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		//create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			Instructor instructor = new Instructor("John", "Doe", "john@doe.com");
+			InstructorDetail instructorDetail = new InstructorDetail("youtube.com/channel", "Code");
+			instructor.setInstructorDetail(instructorDetail);
+			
+			//start a transaction
+			session.beginTransaction();
+			
+			session.save(instructor);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} finally {
+			System.out.println("Closing the connection.");
+			factory.close();
+		}
+
+	}
+
+}
