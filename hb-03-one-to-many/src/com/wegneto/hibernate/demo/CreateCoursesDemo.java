1def419868c61083364dc4a5315b6739ac0c27d3
Wilson Guimarães <wegneto@gmail.com>
2019-11-14 21:28:59 +0000

    Lecture 229. @OneToMany - Bi-Directional - Create Courses for Instructor

---
+++ hb-03-one-to-many/src/com/wegneto/hibernate/demo/CreateCoursesDemo.java
@@ -0,0 +1,55 @@
+package com.wegneto.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.wegneto.hibernate.demo.entity.Course;
+import com.wegneto.hibernate.demo.entity.Instructor;
+import com.wegneto.hibernate.demo.entity.InstructorDetail;
+
+public class CreateCoursesDemo {
+
+	public static void main(String[] args) {
+		//create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//start a transaction
+			session.beginTransaction();
+			
+			int id = 1;
+			Instructor instructor = session.get(Instructor.class, id);
+			
+			Course course1 = new Course("course 1");
+			Course course2 = new Course("course 2");
+			
+			instructor.add(course1);
+			instructor.add(course2);
+			
+			session.save(course1);
+			session.save(course2);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Closing the connection.");
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
