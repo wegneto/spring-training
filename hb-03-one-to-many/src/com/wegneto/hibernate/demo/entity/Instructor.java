35623a6a98abb2f063aa09f68d3fbb4675ea399d
Wilson Guimarães <wegneto@gmail.com>
2019-11-14 21:18:41 +0000

    Lecture 227. @OneToMany - Bi-Directional - Update Instructor

--- hb-03-one-to-many/src/com/wegneto/hibernate/demo/entity/Instructor.java
+++ hb-03-one-to-many/src/com/wegneto/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.wegneto.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -30,6 +34,10 @@ public class Instructor {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
+
+	@OneToMany(mappedBy = "instructor", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH })
+	private List<Course> courses;
 
 	public Instructor() {
 	}
@@ -79,6 +87,22 @@ public class Instructor {
 
 	public void setInstructorDetail(InstructorDetail instructorDetail) {
 		this.instructorDetail = instructorDetail;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	public void add(Course course) {
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		courses.add(course);
+		course.setInstructor(this);
 	}
 
 	@Override
