44d04bfba011f4038df1bd1acc1fcf481374b8f4
Wilson Guimarães <wegneto@gmail.com>
2019-12-05 22:27:38 +0000

    Lecture 245. @OneToMany - Uni-Directional - Configure Fetch Type

--- hb-04-one-to-many-uni/src/com/wegneto/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/com/wegneto/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.wegneto.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,6 +29,10 @@ public class Course {
 	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
 
 	public Course() {
 	}
@@ -54,6 +63,22 @@ public class Course {
 
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public void addReview(Review review) {
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+
+		reviews.add(review);
 	}
 
 	@Override
