ad4193b5a1569f28c6fc1f5ff49aa9d6c5a3b67a
Wilson Guimarães <wegneto@gmail.com>
2020-03-13 23:50:01 +0000

    test 2

--- hb-05-many-to-many/src/com/wegneto/hibernate/demo/CreateCourseAndStudentsDemo.java
+++ hb-05-many-to-many/src/com/wegneto/hibernate/demo/CreateCourseAndStudentsDemo.java
@@ -50,6 +50,7 @@ public class CreateCourseAndStudentsDemo {
 			session.getTransaction().commit();
 
 			System.out.println("Done!");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
