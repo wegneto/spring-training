6bd236a4d96bf2e277474a44c019fb1381fbf126
Wilson Guimarães <wegneto@gmail.com>
2020-03-15 23:45:01 +0000

    259. @ManyToMany - Get Courses for Student

---
+++ hb-05-many-to-many/src/com/wegneto/hibernate/demo/GetCoursesForStudentDemo.java
@@ -0,0 +1,50 @@
+package com.wegneto.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.wegneto.hibernate.demo.entity.Course;
+import com.wegneto.hibernate.demo.entity.Instructor;
+import com.wegneto.hibernate.demo.entity.InstructorDetail;
+import com.wegneto.hibernate.demo.entity.Review;
+import com.wegneto.hibernate.demo.entity.Student;
+
+public class GetCoursesForStudentDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).addAnnotatedClass(Student.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// start a transaction
+			session.beginTransaction();
+
+			Course course = new Course("Course " + System.currentTimeMillis());
+
+			// save the course
+			System.out.println("Saving the course..");
+			session.save(course);
+			System.out.println("Saved the course: " + course);
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Closing the connection.");
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
