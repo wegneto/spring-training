5d3a596a4af1f21c97685f6997a8bc146c799a32
Wilson Guimarães <wegneto@gmail.com>
2020-04-17 17:06:10 +0100

    266. Test Database Connection - Part 2

--- web-customer-tracker/src/com/wegneto/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/wegneto/testdb/TestDbServlet.java
@@ -1,6 +1,9 @@
 package com.wegneto.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,8 +22,31 @@ public class TestDbServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//setup connection variables
+		String user = "root";
+		String password = "root";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		//get connection to database
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to database: " + jdbcUrl);
+			
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(jdbcUrl, user, password);
+			out.println("Connection successful");
+			
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
 	}
+
 }
