adb2d15320076bd05d17c7b3613443b678624eb1
Wilson Guimarães <wegneto@gmail.com>
2020-09-03 22:34:10 +0100

    Lesson 295. Add Customer - Creating the HTML Form

--- web-customer-tracker/src/com/wegneto/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/wegneto/springdemo/controller/CustomerController.java
@@ -31,6 +31,9 @@ public class CustomerController {
 	
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model model) {
+		Customer customer = new Customer();
+		model.addAttribute("customer", customer);
+		
 		return "customer-form";
 	}
 
