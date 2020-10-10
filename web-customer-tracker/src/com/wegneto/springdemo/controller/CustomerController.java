8ef078734a6a8e39198558bc6e5ed0c050aa5c3a
Wilson Guimarães <wegneto@gmail.com>
2020-10-10 21:05:04 +0100

    Lecture 306. Update Customer - Prepopulating the Form

--- web-customer-tracker/src/com/wegneto/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/wegneto/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.wegneto.springdemo.entity.Customer;
 import com.wegneto.springdemo.service.CustomerService;
@@ -30,20 +31,28 @@ public class CustomerController {
 
 		return "list-customers";
 	}
-	
+
 	@GetMapping("/showFormForAdd")
 	public String showFormForAdd(Model model) {
 		Customer customer = new Customer();
 		model.addAttribute("customer", customer);
-		
+
 		return "customer-form";
 	}
-	
+
 	@PostMapping("/saveCustomer")
 	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
 		customerService.saveCustomer(customer);
-		
+
 		return "redirect:/customer/list";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForAdd(@RequestParam("customerId") int customerId, Model model) {
+		Customer customer = customerService.getCustomer(customerId);
+		model.addAttribute("customer", customer);
+
+		return "customer-form";
 	}
 
 }
--- web-customer-tracker/src/com/wegneto/springdemo/service/CustomerService.java
+++ web-customer-tracker/src/com/wegneto/springdemo/service/CustomerService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import com.wegneto.springdemo.entity.Customer;
 
 public interface CustomerService {
-	
+
 	List<Customer> getCustomers();
 
 	void saveCustomer(Customer customer);
+
+	Customer getCustomer(int customerId);
 
 }
