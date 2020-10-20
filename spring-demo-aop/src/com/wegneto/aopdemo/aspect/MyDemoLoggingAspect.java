29e6740412c5050057afcfad083f6df826efef32
Wilson Guimarães <wegneto@gmail.com>
2020-10-20 14:17:26 +0100

    Lecture 327. AOP - Pointcut Expressions - Match only DAO addAccount

--- spring-demo-aop/src/com/wegneto/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/wegneto/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 	
-	@Before("execution(public void addAccount())")
+	@Before("execution(public void com.wegneto.aopdemo.dao.AccountDAO.addAccount())")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
 	}
