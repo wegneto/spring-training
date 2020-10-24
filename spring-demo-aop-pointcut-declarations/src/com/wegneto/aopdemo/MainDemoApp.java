05aa0ce2428c520366df63bfaf9a189f4f44f92d
Wilson Guimarães <wegneto@gmail.com>
2020-10-24 20:45:17 +0100

    Lecture 333. AOP: Pointcut Declarations

---
+++ spring-demo-aop-pointcut-declarations/src/com/wegneto/aopdemo/MainDemoApp.java
@@ -0,0 +1,31 @@
+package com.wegneto.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.wegneto.aopdemo.dao.AccountDAO;
+import com.wegneto.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+		// call the business method
+		Account account = new Account();
+		accountDAO.addAccount(account, true);
+		accountDAO.doSomething();
+
+		membershipDAO.addMemberAccount();
+		membershipDAO.doSomethingElse();
+
+		// close the context
+		context.close();
+	}
+
+}
---
+++ spring-demo-aop-pointcut-declarations/src/com/wegneto/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,19 @@
+package com.wegneto.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.wegneto.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount(Account account, boolean someBoolean) {
+		System.out.println(getClass() + ": ADDING ACOUNT...");
+	}
+	
+	public boolean doSomething() {
+		System.out.println(getClass() + ": DOING SOMETHING...");
+		return false;
+	}
+
+}
