44ec9f3f7cf80c32a613349a7946b8cb476894ca
Wilson Guimarães <wegneto@gmail.com>
2020-10-25 16:11:03 +0000

    Lecture 335. AOP: Combining Pointcuts

---
+++ spring-demo-aop-pointcut-declarations-combination/src/com/wegneto/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,25 @@
+package com.wegneto.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	
+	@Pointcut("execution(* com.wegneto.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Before("forDaoPackage()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=====>>> Executing @Before advice on com.wegneto.aopdemo.dao.*.*(..)");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performSomeOperation() {
+		System.out.println("=====>>> Performing some operation");
+	}
+
+}
