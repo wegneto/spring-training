c89d146e0573a05e898099d23100e2806a276a5d
Wilson Guimarães <wegneto@gmail.com>
2020-11-01 15:52:43 +0000

    Lecture 343. AOP: @AfterReturning - adding new method to AccountDAO

--- spring-demo-aop-after-returning/src/com/wegneto/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-after-returning/src/com/wegneto/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.wegneto.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.wegneto.aopdemo.Account;
@@ -37,6 +40,30 @@ public class AccountDAO {
 	public void setServiceCode(String serviceCode) {
 		System.out.println(getClass() + ".setServiceCode()");
 		this.serviceCode = serviceCode;
+	}
+	
+	public List<Account> findAccounts() {
+		List<Account> result = new ArrayList<Account>();
+		
+		//create sample accounts
+		Account acct1 = new Account();
+		acct1.setName("Name 1");
+		acct1.setLevel("Level 1");
+		
+		Account acct2 = new Account();
+		acct1.setName("Name 2");
+		acct1.setLevel("Level 2");
+		
+		Account acct3 = new Account();
+		acct1.setName("Name 3");
+		acct1.setLevel("Level 3");
+		
+		//add them to the list
+		result.add(acct1);
+		result.add(acct2);
+		result.add(acct3);
+		
+		return result;
 	}
 
 }
