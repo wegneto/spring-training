f6fe157da9611cd96beb896ac98273989565121d
Wilson Guimarães <wegneto@gmail.com>
2020-11-04 15:32:55 +0000

    Lecture 347. AOP: @AfterThrowing

---
+++ spring-demo-aop-after-throwing/src/com/wegneto/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,36 @@
+package com.wegneto.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.wegneto.aopdemo.dao.AccountDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		List<Account> accounts = null;
+		
+		try {
+			accounts = accountDAO.findAccounts();
+		} catch (Exception e) {
+			System.out.println("\n\nMain Program: catch exception: " + e.getMessage());
+		}
+		
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+		System.out.println("----");
+		
+		System.out.println(accounts);
+		
+		System.out.println("\n");
+
+		// close the context
+		context.close();
+	}
+}
