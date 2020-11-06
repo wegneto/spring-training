32cf01cf1b930af9c1e1ecbc685882b77bc9ab77
Wilson Guimarães <wegneto@gmail.com>
2020-11-06 19:31:53 +0000

    Lecture 350. AOP: @After - part 1

--- spring-demo-aop-after-advice/src/com/wegneto/aopdemo/AfterDemoApp.java
+++ spring-demo-aop-after-advice/src/com/wegneto/aopdemo/AfterDemoApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.wegneto.aopdemo.dao.AccountDAO;
 
-public class AfterThrowingDemoApp {
+public class AfterDemoApp {
 
 	public static void main(String[] args) {
 		// read spring config java class
